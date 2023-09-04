ef265b499254b1da7eb3a459f8b33c942bfee11e
Alan <alan.melgar.f@gmail.com>
2023-09-03 21:13:04 -0400

    routes

---
+++ modules/routes/employees.js
@@ -0,0 +1,30 @@
+const {Router} = require('express');
+
+const {
+   get,
+   gets,
+   post,
+   put,
+   del
+
+} = require('../controllers/employees');
+
+const router = Router();
+
+// llama a todos los datos
+router.get('/',gets);
+
+//obtiene a un empleado 
+router.get('/:id', get);
+
+// crea un dato
+router.post('/', post);
+
+// actualiza un dato
+router.put('/:id', put);
+
+//elimina un dato permanentemente 
+router.delete('/:id', del);
+
+module.exports = router;
+
